5b577a5d4a7ee39d0c93452f30aa07772f827d51
JennieLust <94746740+JennieLust@users.noreply.github.com>
2022-08-01 14:15:51 +0200

    fix bugs

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ basic html
 equal button / function
 display that shows display value
 clear button / function
-working calculator */
-
-/* TODO:
-make it so that you can press another operand for a sum instead of HAVING to pick eq each time
-figure out what stops string of operations from working
-add rounding function to long decimals instead of scroll-wheel
-add error when pressing = before all numbers */
-
-/* OPTIONAL: make work with decimals (sounds like hell) */
+working calculator 
+add error when pressing = before all numbers
+add error when dividing by 0 */
 /* OPTIONAL: add backspace button (should be easy, use arr.pop) */
+/* make it so that you can press another operand for a sum instead of HAVING to pick eq each time
+figure out what stops string of operations from working*/
 /* OPTIONAL: add keyboard support (sounds fun and useful) */
 
+
+/* TODO:
+figure out why keyboard support gets wrong sums
+OPTIONAL: make work with decimals (sounds difficult - something to come back to)*/
 
 
 /* init HTML vars */
@@ -43,6 +43,7 @@ const btnAdd = document.querySelector('#op-add');
 const btnSub = document.querySelector('#op-sub');
 const btnMul = document.querySelector('#op-mul');
 const btnDiv = document.querySelector('#op-div'); 
+const btnCOne = document.querySelector('#clear-one-button');
 
 /* vars for storage of numbers */
 let currentArr = [];
@@ -75,6 +76,84 @@ function operate(operator, num1, num2) {
         alert("Unvalid operator")
     };
 ;}
+
+/* keyboard input triggering button events */
+document.body.addEventListener('keydown', (event) => {
+    switch (event.keyCode) {
+        case 96:
+        case 48:
+            btn0.click();
+            break;
+        case 49:
+        case 97:
+            btn1.click();
+            break;
+        case 98:
+        case 50:
+            btn2.click();
+            break;
+        case 99:
+        case 51:
+            btn3.click();
+            break;
+        case 100:
+        case 52:
+            btn4.click();
+            break;
+        case 101:
+        case 53:
+            btn5.click();
+            break;
+        case 102:
+        case 54:
+            btn6.click();
+            break;
+        case 103:
+        case 55:
+            btn7.click();
+            break;
+        case 104:
+        case 56: 
+            btn8.click();
+            break;
+        case 105:
+        case 57:
+            btn9.click();
+            break;
+        case 107:
+        case 170:
+            btnAdd.click();
+            break;
+        case 109:
+        case 173:
+            btnSub.click();
+            break;
+        case 111:
+            btnDiv.click();
+            break;
+        case 106:
+        case 122:
+            btnMul.click();
+            break;
+        case 8: 
+            btnCOne.click();
+            break;
+        case 46:
+            btnC.click();
+            break;
+    }
+    if (event.shiftKey === true && event.keyCode === 55) {
+        btnDiv.click();
+    } else if (event.shiftKey === true && event.keyCode === 48) {
+        /* This results in 
+        1 + 1 = 11 
+        7 / 7 = 0.1
+        1 * 3 = 30
+        10 - 5 = -40
+        WHY */
+        btnEq.click();
+    }
+})
 
 /* number buttons w/ event functions */
 btn0.addEventListener('click', () => {
@@ -146,7 +225,17 @@ btnC.addEventListener('click', () => {
     storedArr = [];
     currentArr = [];
     sum = 0;
-    chngNumBtns("enable")
+    changeButtons("enable")
+})
+
+btnCOne.addEventListener('click', () => {
+    if (currentArr.length >= 1) {    
+        currentArr.pop();
+        displayCurrArr();} 
+    else if (currentArr.length = 0) {
+        btnC.click();
+    }
+
 })
 
 /* operand buttons w/ event functions */
@@ -154,15 +243,13 @@ btnC.addEventListener('click', () => {
 btnAdd.addEventListener('click', () => {
     /* decides if this is a new operation or string operation */
     if (storedArr.length >= 1) {
-        /* calls equal button event function */
         btnEq.click();
         currentOp = "+";
-        /* SOMETHING HERE ISN'T COOPERATING */ 
         displayCurrOp();
         displayCurrArr();
         displayStoArr();
-        currentArr = [];
         storedArr = currentArr
+        currentArr = [];
     } else {
         storedArr = currentArr;
         currentArr = [];
@@ -170,7 +257,6 @@ btnAdd.addEventListener('click', () => {
         displayCurrOp();
         displayCurrArr();
         displayStoArr(); 
-        chngNumBtns("enable");
     }})
 
 btnSub.addEventListener('click', () => {
@@ -180,8 +266,8 @@ btnSub.addEventListener('click', () => {
         displayCurrOp();
         displayCurrArr();
         displayStoArr();
-        currentArr = [];
         storedArr = currentArr
+        currentArr = [];
     } else {
         storedArr = currentArr;
         currentArr = [];
@@ -189,7 +275,6 @@ btnSub.addEventListener('click', () => {
         displayCurrOp();
         displayCurrArr();
         displayStoArr(); 
-        chngNumBtns("enable");
     }})
 
 btnDiv.addEventListener('click', () => {
@@ -199,8 +284,8 @@ btnDiv.addEventListener('click', () => {
         displayCurrOp();
         displayCurrArr();
         displayStoArr();
-        currentArr = [];
         storedArr = currentArr
+        currentArr = [];
     } else {
         storedArr = currentArr;
         currentArr = [];
@@ -208,7 +293,6 @@ btnDiv.addEventListener('click', () => {
         displayCurrOp();
         displayCurrArr();
         displayStoArr(); 
-        chngNumBtns("enable");
     }})
 
 btnMul.addEventListener('click', () => {
@@ -218,8 +302,8 @@ btnMul.addEventListener('click', () => {
         displayCurrOp();
         displayCurrArr();
         displayStoArr();
-        currentArr = [];
         storedArr = currentArr
+        currentArr = [];
     } else {
         storedArr = currentArr;
         currentArr = [];
@@ -227,26 +311,37 @@ btnMul.addEventListener('click', () => {
         displayCurrOp();
         displayCurrArr();
         displayStoArr(); 
-        chngNumBtns("enable");
     }})
 
 btnEq.addEventListener('click', () => {
-    if (currentOp === "+") {
-        sum = add(parseInt(storedArr.join("")), parseInt(currentArr.join(""))) 
-    } else if (currentOp === "-") {
-        sum = subtract(parseInt(storedArr.join("")), parseInt(currentArr.join("")))
-    } else if (currentOp === "/") {
-        sum = divide(parseInt(storedArr.join("")), parseInt(currentArr.join("")))
-    } else if (currentOp === "*") {
-        sum = multiply(parseInt(storedArr.join("")), parseInt(currentArr.join("")))
-    } else if (currentArr.length < 1 && storedArr.length < 1) {
-        sum = 0
-    } else if (currentArr.length < 1 && storedArr.length >= 1) {
-        sum = parseInt(storedArr.join(""))
-    } else if (currentArr >= 1 && storedArr.length < 1) {
+    if (currentOp != "" && currentArr.length >= 1) {
+        if (currentOp === "+") {
+            sum = add(parseInt(storedArr.join("")), parseInt(currentArr.join(""))) 
+        } else if (currentOp === "-") {
+            sum = subtract(parseInt(storedArr.join("")), parseInt(currentArr.join("")))
+        } else if (currentOp === "/") {
+            if (parseInt(storedArr.join("")) === 0 || parseInt(currentArr.join("")) === 0) {
+                sum = "nonono"
+                changeButtons("disable")
+            } else {
+                sum = divide(parseInt(storedArr.join("")), parseInt(currentArr.join("")))};
+        } else if (currentOp === "*") {
+            sum = multiply(parseInt(storedArr.join("")), parseInt(currentArr.join("")))
+        } else if (currentArr.length < 1 && storedArr.length < 1) {
+            sum = 0
+        } else if (currentArr.length < 1 && storedArr.length >= 1) {
+            sum = parseInt(storedArr.join(""))
+        } else if (currentArr.length >= 1 && storedArr.length < 1) {
+            
+        } else {
+            sum = 0;
+        }
+    } else if (currentOp === "" && currentArr.length >= 1) {
         sum = parseInt(currentArr.join(""))
     } else {
-        sum = 0;
+        sum = "ERROR";
+        changeButtons("disable")
+
     }
     currentArr = [];
     storedArr = [];
@@ -255,11 +350,52 @@ btnEq.addEventListener('click', () => {
     displayCurrArr()
     displayCurrOp()
     displayStoArr();
-    chngNumBtns("disable");
 })
 
+function changeButtons(action) {
+    if (action === "disable") {
+        btn0.disabled = true
+        btn1.disabled = true
+        btn2.disabled = true
+        btn3.disabled = true
+        btn4.disabled = true
+        btn5.disabled = true
+        btn6.disabled = true
+        btn7.disabled = true
+        btn8.disabled = true
+        btn9.disabled = true
+        btnEq.disabled = true
+        btnAdd.disabled = true
+        btnSub.disabled = true
+        btnMul.disabled = true
+        btnDiv.disabled = true
+    } else if  (action === "enable")  {
+        btn0.disabled = false
+        btn1.disabled = false
+        btn2.disabled = false
+        btn3.disabled = false
+        btn4.disabled = false
+        btn5.disabled = false
+        btn6.disabled = false
+        btn7.disabled = false
+        btn8.disabled = false
+        btn9.disabled = false
+        btnEq.disabled = false
+        btnAdd.disabled = false
+        btnSub.disabled = false
+        btnMul.disabled = false
+        btnDiv.disabled = false
+    }
+};
+
 function displayCurrArr() {
-    calcDisplay.innerHTML = currentArr.join('');
+    if (currentArr.length < 1) {
+        calcDisplay.innerHTML = "" 
+    } else {
+        calcDisplay.innerHTML = Math.round(parseFloat(currentArr.join('')) * 100) / 100;
+    }
+   
+    
 };
 
 function displayStoArr() {
@@ -270,30 +406,5 @@ function displayCurrOp() {
     calcOpDisplay.innerHTML = currentOp;
 }
 
-function chngNumBtns(action) {
-    if (action === "enable") {
-        btn0.disabled = false;
-        btn1.disabled = false;
-        btn2.disabled = false;
-        btn3.disabled = false;
-        btn4.disabled = false;
-        btn5.disabled = false;
-        btn6.disabled = false;
-        btn7.disabled = false;
-        btn8.disabled = false;
-        btn9.disabled = false;
-    } else if (action === "disable") {
-        btn0.disabled = true;
-        btn1.disabled = true;
-        btn2.disabled = true;
-        btn3.disabled = true;
-        btn4.disabled = true;
-        btn5.disabled = true;
-        btn6.disabled = true;
-        btn7.disabled = true;
-        btn8.disabled = true;
-        btn9.disabled = true;
-    }
-}
 
 
